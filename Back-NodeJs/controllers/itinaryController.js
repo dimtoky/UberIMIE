2469fc94b3844015c43f2a77972cbd28011c5b21
Marc JEAN PIERRE <coursuniv.jeanpierre@gmail.com>
2021-01-01 21:10:05 +0100

    👌 IMPROVE: Console itinary indication for dev

--- Back-NodeJs/controllers/itinaryController.js
+++ Back-NodeJs/controllers/itinaryController.js
@@ -5,9 +5,11 @@ module.exports = {
     // @desc  dijsktra algorithm
     // @access Public
     dijkstraTest: async (req, res) => {
+        console.log("Compute itinary...");
         const result = await dijkstra(req.body.start, req.body.coords, req.body.len)
         res.status(200).json({
             data: result
         })
+        console.log("Itinary: OK");
     }
 }
