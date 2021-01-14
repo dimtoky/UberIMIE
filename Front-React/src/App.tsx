205e2a090256382c884ca17309c74f2a4305423d
riadh <riadh.s@fullrama.net>
2021-01-14 17:43:01 +0100

    NEW: history front components

--- Front-React/src/App.tsx
+++ Front-React/src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { LayoutAuth } from "./Layout/LayoutAuth/Layout";
 import { LayoutGame } from "./Layout/LayoutGame/Layout";
+import { LayoutHistory } from "./Layout/LayoutHistory/Layout";
+
 
 export default function App() {
   return (
@@ -10,8 +12,10 @@ export default function App() {
         <Switch>
             <Route path='/auth' component={LayoutAuth.Display} />
             <Route path='/drive' component={LayoutGame.Display} />
+            <Route path='/history' component={LayoutHistory.Display} />
             <Redirect to="/auth/signIn" />
         </Switch>
     </BrowserRouter>
+
   );
 }
