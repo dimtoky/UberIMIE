78c8dd09f1bc82b29c0c7debd6021532602700a1
dimtoky <dimension43@hotmail.fr>
2021-01-16 00:01:50 +0100

    NEW: add https

--- Back-NodeJs/index.js
+++ Back-NodeJs/index.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const router = require('./router').router;
 const cors = require('cors');
-
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+
+
+
+
 
 mongoose.connect(
   "mongodb+srv://root:wamwnz11DmcpxtDq@clusterimie.euvgs.mongodb.net/imiebd?retryWrites=true&w=majority",
@@ -16,6 +22,8 @@ mongoose.connect(
 );
 
 
+// serve the API with signed certificate on 443 (SSL/HTTPS) port
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,10 +34,18 @@ app.use(cors());
 //configure routes
 app.use('/', router);
 
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync(' /etc/letsencrypt/live/uberimiegrp8.ddns.net/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/uberimiegrp8.ddns.net/fullchain.pem'),
+}, app);
 
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
 
-app.listen(3001, () => {
-    console.log('Listening on port 3001');
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
 });
 
 module.exports = app
